cb631cc696a7a6f705c160b145a60dc7f460e60b
ajayarcher <garg.ajay1987@gmail.com>
2016-11-30 18:12:49 +0530

    sql

---
+++ tennis.sql
@@ -0,0 +1,620 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2016 at 01:42 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.5.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tennis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `badges`
+--
+
+CREATE TABLE `badges` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `badges`
+--
+
+INSERT INTO `badges` (`id`, `user_id`, `name`, `image`, `created`, `modified`) VALUES
+(1, 1, 'New Recruit', 'badge.png', '2016-11-30 05:12:11', '2016-11-30 05:11:11'),
+(2, 1, 'Tier Veteran', 'badge (2).png', '2016-11-30 05:12:11', '2016-11-30 05:11:11'),
+(3, 1, 'Tribe Leader', 'badge (3).png', '2016-11-30 05:12:11', '2016-11-30 05:11:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `sortname` varchar(3) NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`id`, `sortname`, `name`) VALUES
+(1, 'AF', 'Afghanistan'),
+(2, 'AL', 'Albania'),
+(3, 'DZ', 'Algeria'),
+(4, 'AS', 'American Samoa'),
+(5, 'AD', 'Andorra'),
+(6, 'AO', 'Angola'),
+(7, 'AI', 'Anguilla'),
+(8, 'AQ', 'Antarctica'),
+(9, 'AG', 'Antigua And Barbuda'),
+(10, 'AR', 'Argentina'),
+(11, 'AM', 'Armenia'),
+(12, 'AW', 'Aruba'),
+(13, 'AU', 'Australia'),
+(14, 'AT', 'Austria'),
+(15, 'AZ', 'Azerbaijan'),
+(16, 'BS', 'Bahamas The'),
+(17, 'BH', 'Bahrain'),
+(18, 'BD', 'Bangladesh'),
+(19, 'BB', 'Barbados'),
+(20, 'BY', 'Belarus'),
+(21, 'BE', 'Belgium'),
+(22, 'BZ', 'Belize'),
+(23, 'BJ', 'Benin'),
+(24, 'BM', 'Bermuda'),
+(25, 'BT', 'Bhutan'),
+(26, 'BO', 'Bolivia'),
+(27, 'BA', 'Bosnia and Herzegovina'),
+(28, 'BW', 'Botswana'),
+(29, 'BV', 'Bouvet Island'),
+(30, 'BR', 'Brazil'),
+(31, 'IO', 'British Indian Ocean Territory'),
+(32, 'BN', 'Brunei'),
+(33, 'BG', 'Bulgaria'),
+(34, 'BF', 'Burkina Faso'),
+(35, 'BI', 'Burundi'),
+(36, 'KH', 'Cambodia'),
+(37, 'CM', 'Cameroon'),
+(38, 'CA', 'Canada'),
+(39, 'CV', 'Cape Verde'),
+(40, 'KY', 'Cayman Islands'),
+(41, 'CF', 'Central African Republic'),
+(42, 'TD', 'Chad'),
+(43, 'CL', 'Chile'),
+(44, 'CN', 'China'),
+(45, 'CX', 'Christmas Island'),
+(46, 'CC', 'Cocos (Keeling) Islands'),
+(47, 'CO', 'Colombia'),
+(48, 'KM', 'Comoros'),
+(49, 'CG', 'Congo'),
+(50, 'CD', 'Congo The Democratic Republic Of The'),
+(51, 'CK', 'Cook Islands'),
+(52, 'CR', 'Costa Rica'),
+(53, 'CI', 'Cote D''Ivoire (Ivory Coast)'),
+(54, 'HR', 'Croatia (Hrvatska)'),
+(55, 'CU', 'Cuba'),
+(56, 'CY', 'Cyprus'),
+(57, 'CZ', 'Czech Republic'),
+(58, 'DK', 'Denmark'),
+(59, 'DJ', 'Djibouti'),
+(60, 'DM', 'Dominica'),
+(61, 'DO', 'Dominican Republic'),
+(62, 'TP', 'East Timor'),
+(63, 'EC', 'Ecuador'),
+(64, 'EG', 'Egypt'),
+(65, 'SV', 'El Salvador'),
+(66, 'GQ', 'Equatorial Guinea'),
+(67, 'ER', 'Eritrea'),
+(68, 'EE', 'Estonia'),
+(69, 'ET', 'Ethiopia'),
+(70, 'XA', 'External Territories of Australia'),
+(71, 'FK', 'Falkland Islands'),
+(72, 'FO', 'Faroe Islands'),
+(73, 'FJ', 'Fiji Islands'),
+(74, 'FI', 'Finland'),
+(75, 'FR', 'France'),
+(76, 'GF', 'French Guiana'),
+(77, 'PF', 'French Polynesia'),
+(78, 'TF', 'French Southern Territories'),
+(79, 'GA', 'Gabon'),
+(80, 'GM', 'Gambia The'),
+(81, 'GE', 'Georgia'),
+(82, 'DE', 'Germany'),
+(83, 'GH', 'Ghana'),
+(84, 'GI', 'Gibraltar'),
+(85, 'GR', 'Greece'),
+(86, 'GL', 'Greenland'),
+(87, 'GD', 'Grenada'),
+(88, 'GP', 'Guadeloupe'),
+(89, 'GU', 'Guam'),
+(90, 'GT', 'Guatemala'),
+(91, 'XU', 'Guernsey and Alderney'),
+(92, 'GN', 'Guinea'),
+(93, 'GW', 'Guinea-Bissau'),
+(94, 'GY', 'Guyana'),
+(95, 'HT', 'Haiti'),
+(96, 'HM', 'Heard and McDonald Islands'),
+(97, 'HN', 'Honduras'),
+(98, 'HK', 'Hong Kong S.A.R.'),
+(99, 'HU', 'Hungary'),
+(100, 'IS', 'Iceland'),
+(101, 'IN', 'India'),
+(102, 'ID', 'Indonesia'),
+(103, 'IR', 'Iran'),
+(104, 'IQ', 'Iraq'),
+(105, 'IE', 'Ireland'),
+(106, 'IL', 'Israel'),
+(107, 'IT', 'Italy'),
+(108, 'JM', 'Jamaica'),
+(109, 'JP', 'Japan'),
+(110, 'XJ', 'Jersey'),
+(111, 'JO', 'Jordan'),
+(112, 'KZ', 'Kazakhstan'),
+(113, 'KE', 'Kenya'),
+(114, 'KI', 'Kiribati'),
+(115, 'KP', 'Korea North'),
+(116, 'KR', 'Korea South'),
+(117, 'KW', 'Kuwait'),
+(118, 'KG', 'Kyrgyzstan'),
+(119, 'LA', 'Laos'),
+(120, 'LV', 'Latvia'),
+(121, 'LB', 'Lebanon'),
+(122, 'LS', 'Lesotho'),
+(123, 'LR', 'Liberia'),
+(124, 'LY', 'Libya'),
+(125, 'LI', 'Liechtenstein'),
+(126, 'LT', 'Lithuania'),
+(127, 'LU', 'Luxembourg'),
+(128, 'MO', 'Macau S.A.R.'),
+(129, 'MK', 'Macedonia'),
+(130, 'MG', 'Madagascar'),
+(131, 'MW', 'Malawi'),
+(132, 'MY', 'Malaysia'),
+(133, 'MV', 'Maldives'),
+(134, 'ML', 'Mali'),
+(135, 'MT', 'Malta'),
+(136, 'XM', 'Man (Isle of)'),
+(137, 'MH', 'Marshall Islands'),
+(138, 'MQ', 'Martinique'),
+(139, 'MR', 'Mauritania'),
+(140, 'MU', 'Mauritius'),
+(141, 'YT', 'Mayotte'),
+(142, 'MX', 'Mexico'),
+(143, 'FM', 'Micronesia'),
+(144, 'MD', 'Moldova'),
+(145, 'MC', 'Monaco'),
+(146, 'MN', 'Mongolia'),
+(147, 'MS', 'Montserrat'),
+(148, 'MA', 'Morocco'),
+(149, 'MZ', 'Mozambique'),
+(150, 'MM', 'Myanmar'),
+(151, 'NA', 'Namibia'),
+(152, 'NR', 'Nauru'),
+(153, 'NP', 'Nepal'),
+(154, 'AN', 'Netherlands Antilles'),
+(155, 'NL', 'Netherlands The'),
+(156, 'NC', 'New Caledonia'),
+(157, 'NZ', 'New Zealand'),
+(158, 'NI', 'Nicaragua'),
+(159, 'NE', 'Niger'),
+(160, 'NG', 'Nigeria'),
+(161, 'NU', 'Niue'),
+(162, 'NF', 'Norfolk Island'),
+(163, 'MP', 'Northern Mariana Islands'),
+(164, 'NO', 'Norway'),
+(165, 'OM', 'Oman'),
+(166, 'PK', 'Pakistan'),
+(167, 'PW', 'Palau'),
+(168, 'PS', 'Palestinian Territory Occupied'),
+(169, 'PA', 'Panama'),
+(170, 'PG', 'Papua new Guinea'),
+(171, 'PY', 'Paraguay'),
+(172, 'PE', 'Peru'),
+(173, 'PH', 'Philippines'),
+(174, 'PN', 'Pitcairn Island'),
+(175, 'PL', 'Poland'),
+(176, 'PT', 'Portugal'),
+(177, 'PR', 'Puerto Rico'),
+(178, 'QA', 'Qatar'),
+(179, 'RE', 'Reunion'),
+(180, 'RO', 'Romania'),
+(181, 'RU', 'Russia'),
+(182, 'RW', 'Rwanda'),
+(183, 'SH', 'Saint Helena'),
+(184, 'KN', 'Saint Kitts And Nevis'),
+(185, 'LC', 'Saint Lucia'),
+(186, 'PM', 'Saint Pierre and Miquelon'),
+(187, 'VC', 'Saint Vincent And The Grenadines'),
+(188, 'WS', 'Samoa'),
+(189, 'SM', 'San Marino'),
+(190, 'ST', 'Sao Tome and Principe'),
+(191, 'SA', 'Saudi Arabia'),
+(192, 'SN', 'Senegal'),
+(193, 'RS', 'Serbia'),
+(194, 'SC', 'Seychelles'),
+(195, 'SL', 'Sierra Leone'),
+(196, 'SG', 'Singapore'),
+(197, 'SK', 'Slovakia'),
+(198, 'SI', 'Slovenia'),
+(199, 'XG', 'Smaller Territories of the UK'),
+(200, 'SB', 'Solomon Islands'),
+(201, 'SO', 'Somalia'),
+(202, 'ZA', 'South Africa'),
+(203, 'GS', 'South Georgia'),
+(204, 'SS', 'South Sudan'),
+(205, 'ES', 'Spain'),
+(206, 'LK', 'Sri Lanka'),
+(207, 'SD', 'Sudan'),
+(208, 'SR', 'Suriname'),
+(209, 'SJ', 'Svalbard And Jan Mayen Islands'),
+(210, 'SZ', 'Swaziland'),
+(211, 'SE', 'Sweden'),
+(212, 'CH', 'Switzerland'),
+(213, 'SY', 'Syria'),
+(214, 'TW', 'Taiwan'),
+(215, 'TJ', 'Tajikistan'),
+(216, 'TZ', 'Tanzania'),
+(217, 'TH', 'Thailand'),
+(218, 'TG', 'Togo'),
+(219, 'TK', 'Tokelau'),
+(220, 'TO', 'Tonga'),
+(221, 'TT', 'Trinidad And Tobago'),
+(222, 'TN', 'Tunisia'),
+(223, 'TR', 'Turkey'),
+(224, 'TM', 'Turkmenistan'),
+(225, 'TC', 'Turks And Caicos Islands'),
+(226, 'TV', 'Tuvalu'),
+(227, 'UG', 'Uganda'),
+(228, 'UA', 'Ukraine'),
+(229, 'AE', 'United Arab Emirates'),
+(230, 'GB', 'United Kingdom'),
+(231, 'US', 'United States'),
+(232, 'UM', 'United States Minor Outlying Islands'),
+(233, 'UY', 'Uruguay'),
+(234, 'UZ', 'Uzbekistan'),
+(235, 'VU', 'Vanuatu'),
+(236, 'VA', 'Vatican City State (Holy See)'),
+(237, 'VE', 'Venezuela'),
+(238, 'VN', 'Vietnam'),
+(239, 'VG', 'Virgin Islands (British)'),
+(240, 'VI', 'Virgin Islands (US)'),
+(241, 'WF', 'Wallis And Futuna Islands'),
+(242, 'EH', 'Western Sahara'),
+(243, 'YE', 'Yemen'),
+(244, 'YU', 'Yugoslavia'),
+(245, 'ZM', 'Zambia'),
+(246, 'ZW', 'Zimbabwe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `email_templates`
+--
+
+CREATE TABLE `email_templates` (
+  `id` int(11) NOT NULL,
+  `slug` varchar(50) NOT NULL,
+  `from_name` varchar(50) NOT NULL,
+  `from_email` varchar(100) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1' COMMENT '1=active, 2=in-active',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `email_templates`
+--
+
+INSERT INTO `email_templates` (`id`, `slug`, `from_name`, `from_email`, `subject`, `description`, `status`, `created`, `modified`) VALUES
+(1, 'forget-password', 'Fablysh', 'no-reply@fablysh.com', 'Temporary Password', 'Hello {USERNAME},\n\nIt seems, you have forgot your password, please login with this temporary password:- {PASSWORD}, after login, don''t forget to change your password.\n\nRegards\n{SITENAME}', 1, '2016-09-27 04:10:09', '2016-09-27 03:08:10'),
+(2, 'verify-account', 'Tennis', 'no-reply@tennis.com', 'Verify Account', 'Hello {USERNAME},\r\n<br><br>\r\nThank you for signing up with us, to use our services you need to verify your account. Please click on below link to verify the account. <br><br>\r\n{VERIFYLINK}\r\n<br><br>\r\nRegards\r\n{SITENAME}', 1, '2016-09-27 04:10:09', '2016-09-27 03:08:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matches`
+--
+
+CREATE TABLE `matches` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `play` varchar(50) NOT NULL,
+  `oponent` varchar(50) NOT NULL,
+  `match_type` varchar(50) NOT NULL,
+  `result` varchar(50) NOT NULL,
+  `date_played` date NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `matches`
+--
+
+INSERT INTO `matches` (`id`, `user_id`, `play`, `oponent`, `match_type`, `result`, `date_played`, `created`, `modified`) VALUES
+(1, 1, 'Scheduled', 'Micheal Adams', 'Singles', 'Won', '2016-11-30', '2016-11-30 06:15:15', '2016-11-30 05:13:12'),
+(2, 1, 'Scheduled', 'Micheal Adams', 'Singles', 'Won', '2016-11-30', '2016-11-30 06:15:15', '2016-11-30 05:13:12'),
+(3, 1, 'Scheduled', 'Micheal Adams', 'Singles', 'Won', '2016-11-30', '2016-11-30 06:15:15', '2016-11-30 05:13:12'),
+(4, 1, 'Scheduled', 'Micheal Adams', 'Singles', 'Won', '2016-11-30', '2016-11-30 06:15:15', '2016-11-30 05:13:12'),
+(5, 1, 'Scheduled', 'Micheal Adams', 'Singles', 'Won', '2016-11-30', '2016-11-30 06:15:15', '2016-11-30 05:13:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `created`, `modified`) VALUES
+(1, 'user', '2016-11-29 04:10:10', '2016-11-29 04:10:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tribes`
+--
+
+CREATE TABLE `tribes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rank` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `leader` varchar(50) NOT NULL,
+  `date_joined` date NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tribes`
+--
+
+INSERT INTO `tribes` (`id`, `user_id`, `rank`, `name`, `leader`, `date_joined`, `created`, `modified`) VALUES
+(1, 1, 1, 'Micheal Adams', 'Roggers Fedrer', '2016-11-30', '2016-11-30 06:15:15', '2016-11-30 05:13:12'),
+(2, 1, 4, 'Micheal Adams', 'Roggers Fedrer', '2016-11-30', '2016-11-30 06:15:15', '2016-11-30 05:13:12'),
+(3, 1, 2, 'Micheal Adams', 'Roggers Fedrer', '2016-11-30', '2016-11-30 06:15:15', '2016-11-30 05:13:12'),
+(4, 1, 3, 'Micheal Adams', 'Roggers Fedrer', '2016-11-30', '2016-11-30 06:15:15', '2016-11-30 05:13:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `verification_code` varchar(50) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=inactive, 1=active',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `verification_code`, `status`, `created`, `modified`) VALUES
+(1, 1, 'agarwalnaveen22', 'b72ddc8052f955461348545d21f85faa3b53d329', '', 1, '2016-11-29 05:13:16', '2016-11-29 06:16:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_detail`
+--
+
+CREATE TABLE `user_detail` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `suffix` varchar(5) NOT NULL,
+  `dob` date NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `postal_code` varchar(15) NOT NULL,
+  `is_rating_public` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=no, 1=yes',
+  `ntrp_rating` varchar(10) DEFAULT NULL,
+  `itn_rating` varchar(10) DEFAULT NULL,
+  `utr_rating` varchar(10) DEFAULT NULL,
+  `coach_rating` varchar(10) NOT NULL,
+  `self_rating` float NOT NULL,
+  `default_mode` enum('Social','Competitive') NOT NULL,
+  `global_rank` int(11) NOT NULL,
+  `band_rank` int(11) NOT NULL,
+  `tribe1_rank` int(11) NOT NULL,
+  `tribe2_rank` int(11) NOT NULL,
+  `is_rank_public` tinyint(1) NOT NULL DEFAULT '0',
+  `is_beaconplay` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=no, 1=yes',
+  `is_challangeplay` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=no, 1=yes',
+  `is_newsletter` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=no, 1=yes',
+  `profile_picture` varchar(255) NOT NULL,
+  `sport_level_name` varchar(50) NOT NULL,
+  `point` varchar(5) NOT NULL,
+  `reliability` float NOT NULL,
+  `is_sport_level_public` tinyint(1) NOT NULL DEFAULT '0',
+  `is_badge_public` tinyint(1) NOT NULL,
+  `preferred_coach` varchar(50) NOT NULL,
+  `preferred_venue` varchar(50) NOT NULL,
+  `preferred_court_surface` varchar(50) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_detail`
+--
+
+INSERT INTO `user_detail` (`id`, `user_id`, `first_name`, `surname`, `suffix`, `dob`, `email`, `country_id`, `postal_code`, `is_rating_public`, `ntrp_rating`, `itn_rating`, `utr_rating`, `coach_rating`, `self_rating`, `default_mode`, `global_rank`, `band_rank`, `tribe1_rank`, `tribe2_rank`, `is_rank_public`, `is_beaconplay`, `is_challangeplay`, `is_newsletter`, `profile_picture`, `sport_level_name`, `point`, `reliability`, `is_sport_level_public`, `is_badge_public`, `preferred_coach`, `preferred_venue`, `preferred_court_surface`, `created`, `modified`) VALUES
+(1, 1, 'naveen', 'agarwal', 'Mr.', '1987-12-09', 'agarwalnaveen22@gmail.com', 101, '302012', 1, '123', '456', '789', '011', 0, 'Social', 5, 4, 6, 8, 1, 0, 1, 1, 'profile-photo_friendly.jpg', 'Tennis', '6300', 40, 1, 0, 'Kiril T.', 'Hamptons', 'Kiril T.', '2016-11-30 06:14:14', '2016-11-30 12:39:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venues`
+--
+
+CREATE TABLE `venues` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` text NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `venues`
+--
+
+INSERT INTO `venues` (`id`, `name`, `address`, `created`, `modified`) VALUES
+(1, 'Selenagar', '114, Selenagar, Malaysia', '2016-11-29 06:14:15', '2016-11-29 05:13:12');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `badges`
+--
+ALTER TABLE `badges`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `email_templates`
+--
+ALTER TABLE `email_templates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `matches`
+--
+ALTER TABLE `matches`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tribes`
+--
+ALTER TABLE `tribes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venues`
+--
+ALTER TABLE `venues`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`);
+ALTER TABLE `venues` ADD FULLTEXT KEY `name_2` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `badges`
+--
+ALTER TABLE `badges`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=247;
+--
+-- AUTO_INCREMENT for table `email_templates`
+--
+ALTER TABLE `email_templates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `matches`
+--
+ALTER TABLE `matches`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tribes`
+--
+ALTER TABLE `tribes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `venues`
+--
+ALTER TABLE `venues`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
